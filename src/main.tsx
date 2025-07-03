8af38a4d7a9e5352f893e639d5f8ba8c8afc4b2c
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-07-03 10:31:52 +0600

    Toast Added

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,20 @@ import { Toaster } from './components/ui/sonner'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <Toaster />
+      <Toaster
+        theme="light"
+        position="top-center"
+        toastOptions={{
+          style: {
+            fontSize: "16px",
+            borderLeft: "4px solid white",
+            background: "#9654f8",
+            color: "#fff",
+            padding: "16px",
+          },
+          className: "shadow-lg rounded-lg",
+        }}
+      />
       <RouterProvider router={router} />
     </Provider>
   </StrictMode>
